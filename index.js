a6d607a69d2868d6edaae7777e3cd4dc8350f407
Anurag Kumar <kumar.anurag00748@gmail.com>
2026-06-14 15:44:26 +0530

    Encapsulate dice roll logic in rollDice function

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-var randomNum1= Math.ceil(6*Math.random());
-var randomNum2= Math.ceil(6*Math.random());
-if (randomNum1>randomNum2){
-    document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
+function rollDice(){
+    var randomNum1= Math.ceil(6*Math.random());
+    var randomNum2= Math.ceil(6*Math.random());
+    if (randomNum1>randomNum2){
+        document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
+    }
+    else if(randomNum1<randomNum2){
+        document.querySelector("h1").innerHTML="Player 2 Wins 🚩";
+    }
+    else{
+        document.querySelector("h1").innerHTML="DRAW!";
+    }
+    document.querySelector(".img1").setAttribute("src","images/dice"+randomNum1+".png");
+    document.querySelector(".img2").setAttribute("src","images/dice"+randomNum2+".png");
 }
-else if(randomNum1<randomNum2){
-    document.querySelector("h1").innerHTML="Player 2 Wins 🚩";
-}
-else{
-    document.querySelector("h1").innerHTML="DRAW!";
-}
-document.querySelector(".img1").setAttribute("src","images/dice"+randomNum1+".png");
-document.querySelector(".img2").setAttribute("src","images/dice"+randomNum2+".png");
-
 window.onload = rollDice;
